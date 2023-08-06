96cfca295d64c1bd19be29beb91cb3c0d5035943
demiddima <demid.dima.1996@gmail.com>
2023-08-07 08:57:35 +1000

    Create model user

---
+++ Fitness.BL/Controller/UserController.cs
@@ -0,0 +1,42 @@
+﻿using Fitness.BL.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fitness.BL.Controller
+{
+    public class UserController
+    {
+        public User User { get; }
+        public UserController(string userName, string genderName, DateTime birthDate, double weight, double height)
+        {
+            var gender = new Gender(genderName);
+            User = new User(userName, gender, birthDate, weight, height);
+        }
+        public UserController() 
+        {
+            var formatter = new BinaryFormatter();
+            using (var fs = new FileStream("users.dat", FileMode.OpenOrCreate))
+            {
+                if (formatter.Deserialize(fs) is User user)
+                {
+                    User = user;
+                }
+                //TODO: Что делать, если пользователя не прочитали?
+            }
+        }
+
+        public void Save()
+        {
+            var formatter = new BinaryFormatter();
+            using (var fs = new FileStream("users.dat", FileMode.OpenOrCreate))
+            {
+                formatter.Serialize(fs, User);
+            }
+        }
+
+    }
+}
