cbf10bb58fe8786d84161ded6b2557f81ffaab99
demiddima <demid.dima.1996@gmail.com>
2023-08-07 08:57:35 +1000

    Create model user and gender. Create UseController

---
+++ Fitness.BL/Model/Gender.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fitness.BL.Model
+{
+    /// <summary>
+    /// Gender.
+    /// </summary>
+    public class Gender
+    {
+        /// <summary>
+        /// Name gender.
+        /// </summary>
+        public string Name { get; }
+        /// <summary>
+        /// Create new gender
+        /// </summary>
+        /// <param name="name">Name gender</param>
+        /// <exception cref="ArgumentNullException"></exception>
+        public Gender(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentNullException("Название пола не может быть пустым или null");
+            }
+        }
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
---
+++ Fitness.CMD/Program.cs
@@ -0,0 +1,30 @@
+﻿using Fitness.BL.Controller;
+
+namespace Fitness.CMD
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Вас приветствует приложение Fitness.");
+
+            Console.WriteLine("Введите имя пользователя: ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Введите пол");
+            string gender = Console.ReadLine();
+
+            Console.WriteLine("Введите дату рождения");
+            DateTime birthDate = DateTime.Parse(Console.ReadLine()); //TODO: TryParse
+
+            Console.WriteLine("Введите вес");
+            double weight = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите рост");
+            double height = double.Parse(Console.ReadLine());
+
+            UserController userController = new UserController(name, gender, birthDate, weight, height);
+            userController.Save();
+        }
+    }
+}
